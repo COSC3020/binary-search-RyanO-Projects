7a488fba5a3fa2491bbaab2b4b2a68146538ebee
Ryan <106790344+RyanO-Projects@users.noreply.github.com>
2024-09-04 16:24:11 -0600

    Infinite Loop
    Oops

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ function binarySearch(list, element) {
     // Implement a binary search function that, given a sorted list and an element
     // to look for, returns the index of the element in the list or -1 if the element is not there.
     var i = 0;
-    while(list[i] != element) {
+    while(list[i] != element && i < list.length) {
         i++;
     }
     console.log(i);
