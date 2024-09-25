6d2c88251f62ff6890e9d7ccc6c46d2836dca465
Ryan <106790344+RyanO-Projects@users.noreply.github.com>
2024-09-24 19:04:53 -0600

    Implemented binary search
    Redid the problem to use an iterative implementation of binary search.

--- code.js
+++ code.js
@@ -1,11 +1,33 @@
 function binarySearch(list, element) {
     // Implement a binary search function that, given a sorted list and an element
     // to look for, returns the index of the element in the list or -1 if the element is not there.
-    var i = 0;
-    while(list[i] != element && i < list.length) {
-        i++;
+    
+    // Determine middle element, compare element to middle, if element == middle then return middle.
+    // if (element < middle), look in first half of array. elif (element > middle), look in second half of array.
+    // Loop logic:
+    //  assign middle, compare middle to element.
+    //  if (element < middle) {narrow to [first - (middle-1)], rinse and repeat: assign new middle value, compare element to new middle}
+    //  elif (element > middle) {narrow to [(middle+1) - last], rinse and repeat: assign new middle value, compare element to new middle}
+    var first = 0;
+    var last = list.length - 1;
+    var found = False;
+
+    while(!found){
+        var middle = math.floor((first + last) / 2);
+
+        if (element == list[middle]) {
+            found = True;
+        }
+        else if (element < list[middle]) {
+            last = middle - 1;
+        }
+        else {
+            first = middle + 1;
+        }
     }
-    console.log(i);
-    if(list[i] == element) { return i; }
+
+    if(found){
+        return middle;
+    }
     return -1;
 }
